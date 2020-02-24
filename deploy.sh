55ef19edd2c23a9d45930caa96edf404a4482ca7
NightWolf1298 <nightwolf1298@gmail.com>
2020-02-24 17:50:55 +0000

    Travis #2

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+    exit 0
+fi
+
+set -e
+
+rm -rf _site
+mkdir _site
+
+git clone https://${GITHUB_TOKEN}@github.com/KovuTheHusky/nightwolf1298.com.git --branch gh-pages _site
+
+yarn
+bundle exec jekyll build
+
+cd _site
+#ruby places.rb ${FOURSQUARE_TOKEN}
+git config user.email "nightwolf1298@gmail.com"
+git config user.name "NightWolf1298"
+git add --all
+git rm -rf *.exe
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --force origin gh-pages
---
+++ pokemongo.rb
@@ -0,0 +1,121 @@
+require 'json'
+
+source = 'node_modules/pokemon-icons/_icons/SVG'
+
+oldJson = nil
+if (File.exist?('_data/pokedex.json'))
+  oldJson = JSON.parse(File.read('_data/pokedex.json'))
+end
+
+json = {
+  'normal' => {
+    'kanto' => {},
+    'johto' => {},
+    'hoenn' => {},
+    'sinnoh' => {},
+    'unova' => {},
+    'kalos' => {},
+    'alola' => {},
+    'unknown' => {}
+  },
+  'shiny' => {
+    'kanto' => {},
+    'johto' => {},
+    'hoenn' => {},
+    'sinnoh' => {},
+    'unova' => {},
+    'kalos' => {},
+    'alola' => {},
+    'unknown' => {}
+  }
+}
+
+totalCaught = 0
+totalPokedex = 0
+
+Dir.foreach(source) do |entry|
+  if (entry[0] != '.')
+    filename = entry
+    name = filename + ''
+    type = 'normal'
+    if (name.include? '-shiny')
+      type = 'shiny'
+      name.slice! '-shiny'
+    end
+    number = name[0..2].to_i
+    if (number <= 151)
+      region = 'kanto'
+    elsif (number <= 251)
+      region = 'johto'
+    elsif (number <= 386)
+      region = 'hoenn'
+    elsif (number <= 493)
+      region = 'sinnoh'
+    elsif (number <= 649)
+      region = 'unova'
+    elsif (number <= 721)
+      region = 'kalos'
+    elsif (number <= 807)
+      region = 'alola'
+    else
+      region = 'unknown'
+    end
+    name = name[4..-5]
+    variant = 0
+    friendlyName = name
+    if (friendlyName.include? '-alola')
+      variant = 1
+      friendlyName.slice! '-alola'
+    end
+    if (friendlyName.include? 'unown-')
+      friendlyName.gsub!('-', ' ')
+      if (friendlyName == 'unown exclamation')
+        friendlyName = 'Unown !'
+      elsif (friendlyName == 'unown question')
+        friendlyName = 'Unown ?'
+      end
+    elsif (friendlyName == 'nidoran-f')
+      friendlyName = 'nidoran ♀'
+    elsif (friendlyName == 'nidoran-m')
+      friendlyName = 'nidoran ♂'
+    elsif (friendlyName == 'mrmime')
+      friendlyName = "Mr. Mime"
+    elsif (friendlyName == 'mimejr')
+      friendlyName = 'Mime Jr.'
+    elsif (friendlyName == 'ho-oh' || friendlyName == 'porygon-z')
+      # do nothing
+    else
+      friendlyName = friendlyName.split(/-/, 2).first
+    end
+    friendlyName = friendlyName.gsub(/(\w+)/) {|s| s.capitalize}
+    caught = 0
+    if (oldJson != nil && oldJson[type][region][entry] && oldJson[type][region][entry]['caught'] == 1)
+      caught = 1
+    end
+    json[type][region][entry] = {
+      'number' => number,
+      'name' => friendlyName,
+      'variant' => variant,
+      'caught' => caught
+    }
+    totalPokedex = totalPokedex + 1
+    totalCaught = totalCaught + caught
+  end
+end
+
+file = File.open("_data/pokemongo.json", "w")
+file.puts(JSON.pretty_generate({
+  'caught' => totalCaught,
+  'total' => totalPokedex
+}))
+file.close
+
+json.each do |k1,type|
+  type.each do |k2,region|
+    json[k1][k2] = region.sort_by { |k,v| [k.scan(/\d+/).first.to_i, v['name'], v['variant'], k] }.to_h
+  end
+end
+
+file = File.open("_data/pokedex.json", "w")
+file.puts(JSON.pretty_generate(json))
+file.close
