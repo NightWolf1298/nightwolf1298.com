e3f9631400be8044c81cacd66bdf97ac430602d1
NightWolf1298 <nightwolf1298@gmail.com>
2020-03-29 18:19:00 -0400

    Track male/female/genderless captures, other forms

--- pokemongo.rb
+++ pokemongo.rb
@@ -35,7 +35,7 @@ simpleData = nil
 if (File.exist?('_data/simple-pokedex.csv'))
   simpleData = {}
  CSV.foreach('_data/simple-pokedex.csv', headers: true) do |row|
-    simpleData[row[1]] = row[0]
+    simpleData[row['IconFile']] = { 'caught' => row['Caught'], 'caught_m' => row['CaughtM'],'caught_f' => row['CaughtF'] }
   end
 end
 
@@ -45,16 +45,7 @@ totalPokedex = 0
 Dir.foreach(source) do |entry|
   if (entry[0] != '.')
     filename = entry
-    if (filename.start_with?('-'))
-      # workaround for '-636-larvesta-shiny.svg' file in latest icon version
-      filename = filename.delete_prefix('-')
-    end
     name = filename + ''
-    type = 'normal'
-    if (name.include? '-shiny')
-      type = 'shiny'
-      name.slice! '-shiny'
-    end
     number = name[0..2].to_i
     if (number <= 151)
       region = 'kanto'
@@ -73,47 +64,94 @@ Dir.foreach(source) do |entry|
     else
       region = 'unknown'
     end
+
+    m = /\d{3}-(?<name>[^-\.]+)(-(?<gender>[fm]))?(-(?<form>[^-\.]+))??(-(?<shiny>shiny))?\.svg/.match(name)
+    if (m.nil?)
+      puts 'NO MATCH: ' + name
+      next
+    end
+
+    friendlyName = m['name']
+    gender = m['gender']
+    shiny = m['shiny']
+    form = m['form']
     name = name[4..-5]
-    variant = 0
-    friendlyName = name
-    if (friendlyName.include? '-alola')
-      variant = 1
-      friendlyName.slice! '-alola'
+
+    case friendlyName
+      when 'unown'
+        friendlyName += case form
+        when 'exclamation'
+          ' !'
+        when 'question'
+          ' ?'
+        when nil
+          ''
+        else
+          ' ' + form
+        end
+        form = nil
+      when 'nidoran', 'hippopotas', 'hippowdon'
+        friendlyName += case gender
+        when 'f'
+          ' ♀'
+        when 'm'
+          ' ♂'
+        else
+          ''
+        end
+      when 'mrmime'
+        'Mr. Mime'
+      when 'mimejr'
+        'Mime Jr.'
+      when 'ho'
+        friendlyName = 'Ho-Oh'
+        form = nil
+      when 'porygon'
+        friendlyName += case form
+        when 'z'
+          '-Z'
+        else
+          ''
+        end
+        form = nil
+      when 'shellos', 'gastrodon'
+        form += ' sea'
+      when 'spinda'
+        form = '#' + form
+      when 'basculin'
+        form += ' stripe'
     end
-    if (friendlyName.include? 'unown-')
-      friendlyName.gsub!('-', ' ')
-      if (friendlyName == 'unown exclamation')
-        friendlyName = 'Unown !'
-      elsif (friendlyName == 'unown question')
-        friendlyName = 'Unown ?'
-      end
-    elsif (friendlyName == 'nidoran-f')
-      friendlyName = 'nidoran ♀'
-    elsif (friendlyName == 'nidoran-m')
-      friendlyName = 'nidoran ♂'
-    elsif (friendlyName == 'mrmime')
-      friendlyName = "Mr. Mime"
-    elsif (friendlyName == 'mimejr')
-      friendlyName = 'Mime Jr.'
-    elsif (friendlyName == 'ho-oh' || friendlyName == 'porygon-z')
-      # do nothing
-    else
-      friendlyName = friendlyName.split(/-/, 2).first
-    end
+
+    variant = (form.nil? ? 'normal' : form).gsub(/(\w+)/) {|s| s.capitalize}
+    type = shiny.nil? ? 'normal' : 'shiny'
     friendlyName = friendlyName.gsub(/(\w+)/) {|s| s.capitalize}
+    # puts [name, friendlyName, gender, shiny, form, variant, type].join(' -- ')
+
     caught = 0
-    if (oldJson != nil && oldJson[type][region][entry] && oldJson[type][region][entry]['caught'] == 1)
-      caught = 1
+    caught_f = 0
+    caught_m = 0
+    stats = nil
+    if (oldJson != nil && oldJson[type][region][entry])
+      stats = oldJson[type][region][entry]
     end
 
     if (simpleData != nil && simpleData[filename])
-      caught = simpleData[filename] == '1' ? 1 : 0
+      stats = simpleData[filename]
     end
+
+    if (stats != nil)
+      caught = stats['caught'].to_i
+      caught_m = stats['caught_m'].to_i
+      caught_f = stats['caught_f'].to_i
+    end
+
     json[type][region][entry] = {
       'number' => number,
       'name' => friendlyName,
       'variant' => variant,
-      'caught' => caught
+      'caught' => caught,
+      'caught_m' => caught_m,
+      'caught_f' => caught_f
     }
     totalPokedex = totalPokedex + 1
     totalCaught = totalCaught + caught
@@ -129,7 +167,7 @@ file.close
 
 json.each do |k1,type|
   type.each do |k2,region|
-    json[k1][k2] = region.sort_by { |k,v| [k.scan(/\d+/).first.to_i, v['name'], v['variant'], k] }.to_h
+    json[k1][k2] = region.sort_by { |k,v| [k.scan(/\d+/).first.to_i, v['name'], v['variant'].gsub('Alola', '~Alola'), k] }.to_h
   end
 end
 
