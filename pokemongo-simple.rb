349272db824a564f2bd94c49613dcc48046fbf54
NightWolf1298 <nightwolf1298@gmail.com>
2020-03-29 02:24:44 +0000

    Travis #25

---
+++ pokemongo-simple.rb
@@ -0,0 +1,29 @@
+require 'json'
+require 'csv'
+
+caught = Hash.new(0)
+if (File.exist?('_data/simple-pokedex.csv'))
+  CSV.foreach('_data/simple-pokedex.csv', headers: true) do |row|
+    caught[row[1]] = row[0]
+  end
+end
+
+rows = Array.new
+json = JSON.parse(File.read('_data/pokedex.json'))
+json.each do |type_name,type| # normal/shiny
+  type.each do |region_name,region| # kanto/johto/hoenn/sinnoh/unova/kalos/alola/unknown
+    region.each do |icon_file,pokemon| # icon filename
+      # caught has either the old value from the existing csv, or 0 for new keys
+      rows.push([caught[icon_file],icon_file])
+    end
+  end
+end
+
+# sort by number - normal (no form), shiny, other forms, alola, alola-shiny
+rows = rows.sort_by { |k| k[1].delete_suffix('.svg').gsub('-alola', '~alola') }
+
+csv = CSV.open('_data/simple-pokedex.csv', 'wb', headers: ['Caught','IconFile'], write_headers: true) do |csv|
+  rows.each do |row|
+    csv << row
+  end
+end
