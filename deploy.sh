9bc21ff74f02a1c9b07bd16978f4cbff48a50116
KovuTheHusky <kovuthehusky@gmail.com>
2020-05-11 08:36:31 -0400

    Fix building with Travis

--- deploy.sh
+++ deploy.sh
@@ -8,16 +8,10 @@ set -e
 
 rm -rf _site
 mkdir _site
-
 git clone https://${GITHUB_TOKEN}@github.com/NightWolf1298/nightwolf1298.com.git --branch gh-pages _site
-
 bundle exec jekyll build
-
 cd _site
 ruby places.rb ${FOURSQUARE_TOKEN}
-git config user.email "nightwolf1298@gmail.com"
-git config user.name "NightWolf1298"
 git add --all
-git rm -rf *.exe
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git push --force origin gh-pages
